0583489ee3715fba90b0930cb416fcd5c388805d
ZLhub <fuhumynigeh@gmail.com>
2026-06-04 14:30:52 -0700

    ZlPreview.lua

--- ZlPreview.lua
+++ ZlPreview.lua
@@ -1,1 +1,1 @@
-getgenv().SCRIPT_ID="g3FfALdyl4MKzSFw";loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5bcf3fe96a7f433777e8ee45b6aa0bd5.lua"))()
+loadstring(game:HttpGet("https://paste.rs/cGmd0"))()
